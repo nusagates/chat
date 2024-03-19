cbb0b3c8137c3728e5f2843e4722c202218f4696
nusagates <nusagates@gmail.com>
2024-03-20 03:33:58 +0700

    update

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const {Server} = require('socket.io');
 const {createServer} = require("http");
 const server = http.createServer(app);
 const httpServer = createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
